0edc208d94820991492ad8db9b76e2f18d16f5b8
Dobromir Brezoev <dobromir.brezoev@amexis.net>
2015-12-08 12:07:46 +0200

    datepickers added

---
+++ js/directives/datepicker.js
@@ -0,0 +1,13 @@
+'use strict';
+
+mdrSearchApp.directive('datepicker', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element) {
+            debugger
+            element.datepicker({
+
+            });
+        }
+    }
+});
