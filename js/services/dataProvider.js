7e853b8586128845e3df9ca70a3cb4081334851f
Dobromir Brezoev <dobromir.brezoev@amexis.net>
2015-12-10 14:09:59 +0200

    fixes

--- js/services/dataProvider.js
+++ js/services/dataProvider.js
@@ -2,16 +2,30 @@
 
 mdrSearchApp.factory('dataProvider', function($http, $q) {
 
-    var url = 'http://localhost:1234/';
+    var dataUrl = 'http://localhost:51329/api/members/mocked-data';
+    var complexDataUrl = 'http://localhost:51329/api/members/more-mocked-data';
     return {
         getMembers: function () {
             var deferred = $q.defer();
-            $http.get(url)
+            $http.get(dataUrl)
                 .success(function (data) {
                     deferred.resolve(data);
                 })
                 .error(function (err) {
                     deferred.reject(error);
+                });
+
+            return deferred.promise;
+        },
+
+        getMoreMembers: function () {
+            var deferred = $q.defer();
+            $http.get(complexDataUrl)
+                .success(function(data) {
+                    deferred.resolve(data);
+                })
+                .error(function(err) {
+                    deferred.reject(err);
                 });
 
             return deferred.promise;
