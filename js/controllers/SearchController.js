739d43ad91bb3ff0f996f7dbe5ff7cc3a1f8c149
Dobromir Brezoev <dobromir.brezoev@amexis.net>
2015-12-10 14:21:35 +0200

    column defs options

--- js/controllers/SearchController.js
+++ js/controllers/SearchController.js
@@ -847,54 +847,55 @@ mdrSearchApp.controller('SearchController',
         //    "additionalProperty20": "20"
         //}];
 
-        //var shortDataColmnDefs = [
-        //    { field: 'memberName', width: columnWidth },
-        //    { field: 'callDate', width: columnWidth},
-        //    { field: 'callerFirstName', width: columnWidth},
-        //    { field: 'callerLastName', width: columnWidth},
-        //    { field: 'callerRelationship', width: columnWidth},
-        //    { field: 'callStatus', width: columnWidth},
-        //    { field: 'callerPhoneNumber', width: columnWidth},
-        //    { field: 'callDuration', width: columnWidth},
-        //    { field: 'callType', width: columnWidth},
-        //    { field: 'anotherCallProperty', width: columnWidth},
-        //];
+        var columnWidth = '150';
+        var shortDataColmnDefs = [
+            { field: 'memberName', width: columnWidth },
+            { field: 'callDate', width: columnWidth},
+            { field: 'callerFirstName', width: columnWidth},
+            { field: 'callerLastName', width: columnWidth},
+            { field: 'callerRelationship', width: columnWidth},
+            { field: 'callStatus', width: columnWidth},
+            { field: 'callerPhoneNumber', width: columnWidth},
+            { field: 'callDuration', width: columnWidth},
+            { field: 'callType', width: columnWidth},
+            { field: 'anotherCallProperty', width: columnWidth},
+        ];
         //
-        //var longDataColmnDefs = [
-        //    { field: 'memberName', width: columnWidth },
-        //    { field: 'callDate', width: columnWidth},
-        //    { field: 'callerFirstName', width: columnWidth},
-        //    { field: 'callerLastName', width: columnWidth},
-        //    { field: 'callerRelationship', width: columnWidth},
-        //    { field: 'callStatus', width: columnWidth},
-        //    { field: 'callerPhoneNumber', width: columnWidth},
-        //    { field: 'callDuration', width: columnWidth},
-        //    { field: 'callType', width: columnWidth},
-        //    { field: 'anotherCallProperty', width: columnWidth},
-        //    { field: 'additionalProperty1', width: columnWidth},
-        //    { field: 'additionalProperty2', width: columnWidth},
-        //    { field: 'additionalProperty3', width: columnWidth},
-        //    { field: 'additionalProperty4', width: columnWidth},
-        //    { field: 'additionalProperty5', width: columnWidth},
-        //    { field: 'additionalProperty6', width: columnWidth},
-        //    { field: 'additionalProperty7', width: columnWidth},
-        //    { field: 'additionalProperty8', width: columnWidth},
-        //    { field: 'additionalProperty9', width: columnWidth},
-        //    { field: 'additionalProperty10', width: columnWidth},
-        //    { field: 'additionalProperty11', width: columnWidth},
-        //    { field: 'additionalProperty12', width: columnWidth},
-        //    { field: 'additionalProperty13', width: columnWidth},
-        //    { field: 'additionalProperty14', width: columnWidth},
-        //    { field: 'additionalProperty15', width: columnWidth},
-        //    { field: 'additionalProperty16', width: columnWidth},
-        //    { field: 'additionalProperty17', width: columnWidth},
-        //    { field: 'additionalProperty18', width: columnWidth},
-        //    { field: 'additionalProperty19', width: columnWidth},
-        //    { field: 'additionalProperty20', width: columnWidth}
-        //];
+        var longDataColmnDefs = [
+            { field: 'memberName', width: columnWidth },
+            { field: 'callDate', width: columnWidth},
+            { field: 'callerFirstName', width: columnWidth},
+            { field: 'callerLastName', width: columnWidth},
+            { field: 'callerRelationship', width: columnWidth},
+            { field: 'callStatus', width: columnWidth},
+            { field: 'callerPhoneNumber', width: columnWidth},
+            { field: 'callDuration', width: columnWidth},
+            { field: 'callType', width: columnWidth},
+            { field: 'anotherCallProperty', width: columnWidth},
+            { field: 'additionalProperty1', width: columnWidth},
+            { field: 'additionalProperty2', width: columnWidth},
+            { field: 'additionalProperty3', width: columnWidth},
+            { field: 'additionalProperty4', width: columnWidth},
+            { field: 'additionalProperty5', width: columnWidth},
+            { field: 'additionalProperty6', width: columnWidth},
+            { field: 'additionalProperty7', width: columnWidth},
+            { field: 'additionalProperty8', width: columnWidth},
+            { field: 'additionalProperty9', width: columnWidth},
+            { field: 'additionalProperty10', width: columnWidth},
+            { field: 'additionalProperty11', width: columnWidth},
+            { field: 'additionalProperty12', width: columnWidth},
+            { field: 'additionalProperty13', width: columnWidth},
+            { field: 'additionalProperty14', width: columnWidth},
+            { field: 'additionalProperty15', width: columnWidth},
+            { field: 'additionalProperty16', width: columnWidth},
+            { field: 'additionalProperty17', width: columnWidth},
+            { field: 'additionalProperty18', width: columnWidth},
+            { field: 'additionalProperty19', width: columnWidth},
+            { field: 'additionalProperty20', width: columnWidth}
+        ];
 
         $scope.showGrid = true;
-        var columnWidth = '150';
+
         //var shortDataUrl = 'http://localhost:51646/api/members/mocked-data';
         //var complexDataUrl = 'http://localhost:51646/api/members/more-mocked-data';
 
