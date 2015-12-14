42073ac2ff17449d61b07f3682bfac6de06b66d3
Dobromir Brezoev <dobromir.brezoev@amexis.net>
2015-12-14 11:58:00 +0200

    first page removed

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ var mdrSearchApp = angular
     .module('mdrSearchApp', ['ngRoute', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid.resizeColumns'])
     .config(function($routeProvider) {
         $routeProvider
-            .when('/home', {
-                templateUrl: 'templates/home.html'
-            })
+            //.when('/home', {
+            //    templateUrl: 'templates/home.html'
+            //})
             .when('/search', {
                 templateUrl: 'templates/search.html'
             })
-            .otherwise({redirectTo: '/home'});
+            .otherwise({redirectTo: '/search'});
     });
